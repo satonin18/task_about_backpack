c47f5a495383aa99664fad59baba4e7f878d5b7a
satonin18 <35444999+satonin18@users.noreply.github.com>
2019-06-03 15:55:26 +0500

    author satonin18 <35444999+satonin18@users.noreply.github.com> 1559559326 +0500 committer satonin18 <satonin18@mail.ru> 1559796577 +0500
    author satonin18 <35444999+satonin18@users.noreply.github.com> 1559559326 +0500
    committer satonin18 <satonin18@mail.ru> 1559796459 +0500
    
    Initial commit
    
    first commit
    
    created java file
    
    added maven module
    
    defined structure maven app
    
    added dependency spring
    
    add strings in .gitignor
    
    Change structure project
    
    ADDED GENERATED
    
    test
    
    test2
    
    test2
    
    test3
    
    test4
    
    test5
    
    ignor list:+folder target
    
    ignor list:+folder from Maven
    
    remove ignorGit fileS
    
    Input name from keys, Output Hello #name
    
    Revert "Input name from keys, Output Hello #name"
    no in master operation
    
    This reverts commit df0b217b3fd5c284738d746685d1172ab6b8e30c.
    
    none
    
    not:Add name
    
    not:alignment text
    
    small
    
    new log file

---
+++ src/main/java/com/app/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.app;
+
+import java.io.*;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		String name = bufferedReader.readLine();
+		System.out.println("Hello "+ name);
+    }
+}
